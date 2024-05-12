9b5754c1642cd11534ba079c7a3ae388a51557f1
Ankit Kumar <akankitak1701@gmail.com>
2024-05-12 22:32:39 +0530

    redis intro

---
+++ src/string.js
@@ -0,0 +1,11 @@
+const client = require('../client')
+
+async function init() {
+    const result = await client.get("user:1");
+    await client.set("msg:1", "Hello ankit");
+    const result2 = await client.get("msg:1");
+    console.log(result2);
+    console.log("Result -> ", result);
+}
+
+init(); 
